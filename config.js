86fc6300fd115bc1b3f6465c8b3fd3229897d184
jay <landaverde353@gmail.com>
2026-09-11 12:11:55 -0500

    api Added

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // When your AWS API exists, paste its base URL here.
 // Example:
 // window.BABY_SHOWER_API = "https://abc123.execute-api.us-east-1.amazonaws.com";
-window.BABY_SHOWER_API = "";
+window.BABY_SHOWER_API = "https://mucclsre80.execute-api.us-east-1.amazonaws.com/";
